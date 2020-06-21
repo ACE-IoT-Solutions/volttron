909cb2a940624ece9e68a08f12c5097695a7c61b
Andrew <andrew@aceiotsolutions.com>
2020-06-21 12:35:03 -0400

    added version to utils.vip_main call

--- services/core/MQTTHistorian/mqtt_historian/agent.py
+++ services/core/MQTTHistorian/mqtt_historian/agent.py
@@ -43,7 +43,10 @@ import sys
 import time
 import gevent
 
-from volttron.platform.agent.base_historian import BaseHistorian, add_timing_data_to_header
+from volttron.platform.agent.base_historian import (
+    BaseHistorian,
+    add_timing_data_to_header,
+)
 from volttron.platform.agent import utils
 
 from paho.mqtt.client import MQTTv311, MQTTv31
@@ -52,7 +55,7 @@ import paho.mqtt.publish as publish
 
 utils.setup_logging()
 _log = logging.getLogger(__name__)
-__version__ = '0.2'
+__version__ = "0.2"
 
 
 class MQTTHistorian(BaseHistorian):
@@ -62,21 +65,20 @@ class MQTTHistorian(BaseHistorian):
     def __init__(self, config_path, **kwargs):
         config = utils.load_config(config_path)
 
-
         # We pass every optional parameter to the MQTT library functions so they
         # default to the same values that paho uses as defaults.
-        self.mqtt_qos = config.get('mqtt_qos', 0)
-        self.mqtt_retain = config.get('mqtt_retain', False)
+        self.mqtt_qos = config.get("mqtt_qos", 0)
+        self.mqtt_retain = config.get("mqtt_retain", False)
 
-        self.mqtt_hostname = config.get('mqtt_hostname', 'localhost')
-        self.mqtt_port = config.get('mqtt_port', 1883)
-        self.mqtt_client_id = config.get('mqtt_client_id', '')
-        self.mqtt_keepalive = config.get('mqtt_keepalive', 60)
-        self.mqtt_will = config.get('mqtt_will', None)
-        self.mqtt_auth = config.get('mqtt_auth', None)
-        self.mqtt_tls = config.get('mqtt_tls', None)
+        self.mqtt_hostname = config.get("mqtt_hostname", "localhost")
+        self.mqtt_port = config.get("mqtt_port", 1883)
+        self.mqtt_client_id = config.get("mqtt_client_id", "")
+        self.mqtt_keepalive = config.get("mqtt_keepalive", 60)
+        self.mqtt_will = config.get("mqtt_will", None)
+        self.mqtt_auth = config.get("mqtt_auth", None)
+        self.mqtt_tls = config.get("mqtt_tls", None)
 
-        protocol = config.get('mqtt_protocol', MQTTv311)
+        protocol = config.get("mqtt_protocol", MQTTv311)
         if protocol == "MQTTv311":
             protocol = MQTTv311
         elif protocol == "MQTTv31":
@@ -100,52 +102,64 @@ class MQTTHistorian(BaseHistorian):
         return time.mktime(datetime.datetime.now().timetuple())
 
     def publish_to_historian(self, to_publish_list):
-        _log.debug("publish_to_historian number of items: {}"
-                   .format(len(to_publish_list)))
+        _log.debug(
+            "publish_to_historian number of items: {}".format(len(to_publish_list))
+        )
         current_time = self.timestamp()
 
         if self._last_error:
             # if we failed we need to wait 60 seconds before we go on.
             if self.timestamp() < self._last_error + 60:
-                _log.debug('Not allowing send < 60 seconds from failure')
+                _log.debug("Not allowing send < 60 seconds from failure")
                 return
 
         to_send = []
         for x in to_publish_list:
-            topic = x['topic']
+            topic = x["topic"]
 
             # Construct payload from data in the publish item.
             # Available fields: 'value', 'headers', and 'meta'
-            payload = x['value']
+            payload = x["value"]
 
-            to_send.append({'topic': topic,
-                            'payload': payload,
-                            'qos': self.mqtt_qos,
-                            'retain': self.mqtt_retain})
+            to_send.append(
+                {
+                    "topic": topic,
+                    "payload": payload,
+                    "qos": self.mqtt_qos,
+                    "retain": self.mqtt_retain,
+                }
+            )
 
         try:
-            publish.multiple(to_send,
-                             hostname=self.mqtt_hostname,
-                             port=self.mqtt_port,
-                             client_id=self.mqtt_client_id,
-                             keepalive=self.mqtt_keepalive,
-                             will=self.mqtt_will,
-                             auth=self.mqtt_auth,
-                             tls=self.mqtt_tls,
-                             protocol=self.mqtt_protocol)
+            publish.multiple(
+                to_send,
+                hostname=self.mqtt_hostname,
+                port=self.mqtt_port,
+                client_id=self.mqtt_client_id,
+                keepalive=self.mqtt_keepalive,
+                will=self.mqtt_will,
+                auth=self.mqtt_auth,
+                tls=self.mqtt_tls,
+                protocol=self.mqtt_protocol,
+            )
             self.report_all_handled()
         except Exception as e:
-            _log.warning("Exception ({}) raised by publish: {}".format(
-                e.__class__.__name__,
-                e))
+            _log.warning(
+                "Exception ({}) raised by publish: {}".format(e.__class__.__name__, e)
+            )
             self._last_error = self.timestamp()
 
 
 def main(argv=sys.argv):
-    utils.vip_main(MQTTHistorian)
+    try:
+        utils.vip_main(MQTTHistorian, version=__version__)
+    except Exception as e:
+        print(e)
+        _log.exception(e)
+        _log.exception("unhandled exception")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         sys.exit(main())
     except KeyboardInterrupt:
